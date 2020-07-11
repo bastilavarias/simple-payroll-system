1837a5dfa3574a9933e0bc156adc04ead90e2ee2
Basti Lavarias <denzilbao@gmail.com>
2020-07-11 15:22:18 +0800

    [PROGRESS] initial report module design created.

--- web-client/src/pages/Report.vue
+++ web-client/src/pages/Report.vue
@@ -8,6 +8,22 @@
         >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo,
         voluptatum.</v-card-subtitle
       >
+      <v-card-text>
+        <v-row dense>
+          <v-col cols="12" md="5">
+            <v-select label="Date From" outlined></v-select>
+          </v-col>
+          <v-col cols="12" md="5">
+            <v-select label="Date To" outlined></v-select>
+          </v-col>
+          <v-col cols="12" md="2">
+            <v-btn color="primary" x-large block>
+              <span class="mr-1">Process</span>
+              <v-icon x-large>mdi-cogs</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card-text>
     </v-card>
   </section>
 </template>
